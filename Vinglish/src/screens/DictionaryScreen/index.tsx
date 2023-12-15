b1ed6ecb65a09e9ae152356870b28b5fe92af220
Aparna Krishnan <aparna.k@vonnue.com>
2023-12-15 21:12:02 +0530

    feat: add audio

--- Vinglish/src/screens/DictionaryScreen/index.tsx
+++ Vinglish/src/screens/DictionaryScreen/index.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {View, Image, Text, ScrollView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Image, Text, ScrollView, TouchableOpacity} from 'react-native';
 import axios from 'axios';
 import ScreenWrapper from '../../components/ScreenWrapper';
 import SearchBarComponent from '../../components/SearchBar';
@@ -7,10 +7,16 @@ import {styles} from './style';
 import {PageHeader} from '../../components/PageHeader';
 import Spacer from '../../components/Spacer';
 import {DictionaryData} from '../../types/dictionary';
+import Sound from 'react-native-sound';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {FONTSIZES} from '../../themes/font';
+import {COLORS} from '../../themes/colors';
 
 const Dictionary = () => {
   const [inputQuery, setInputQuery] = useState('');
   const [wordData, setWordData] = useState<DictionaryData>([]);
+  const [error, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const dictionaryApiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${inputQuery}`;
 
@@ -22,11 +28,42 @@ const Dictionary = () => {
           setWordData(response.data);
         })
         .catch(error => {
-          console.error('Dictionary API Error:', error);
+          setIsError(true);
+          setErrorMessage(error.response?.data?.title);
         });
   }
 
+  useEffect(() => {
+    if (inputQuery === '') {
+      setWordData([]);
+    }
+  }, [inputQuery]);
+
+  // Sound && Sound.setCategory('Playback');
+
+  // const ding = new Sound(
+  //   'https://api.dictionaryapi.dev/media/pronunciations/en/gentle-us.mp3',
+  //   Sound.MAIN_BUNDLE,
+  //   error => {
+  //     if (error) {
+  //       console.log('error');
+  //       return;
+  //     }
+  //     console.log('duration: ');
+  //   },
+  // );
+
   console.log(wordData);
+
+  const handlePlay = () => {
+    // ding.play(success => {
+    //   if (success) {
+    //     console.log('success');
+    //   } else {
+    //     console.log('error');
+    //   }
+    // });
+  };
 
   return (
     <PageHeader title="Dictionary" variant="PRIMARY">
@@ -39,7 +76,7 @@ const Dictionary = () => {
               placeholder="Search your word..."
               onPress={getData}
             />
-            {wordData.length === 0 ? (
+            {wordData.length === 0 || inputQuery === '' ? (
               <Image
                 source={require('../../assets/images/preview.jpg')}
                 alt="preview"
@@ -50,40 +87,68 @@ const Dictionary = () => {
                 <View>
                   {wordData.map(data => {
                     return (
-                      <>
-                        <View>
-                          <Text>{inputQuery}</Text>
-                          <Text>{data.phonetic}</Text>
-                          {data?.meanings.map(meaning => {
+                      <View key={data.phonetic}>
+                        <View style={styles.dataContainer}>
+                          {/* <Text style={styles.inputQuery}>{inputQuery}</Text> */}
+                          <Text style={styles.phonetic}>{data.phonetic}</Text>
+                          {data?.meanings.map((meaning, index) => {
                             return (
-                              <>
+                              <View key={index}>
                                 <Spacer space={18} />
                                 <Text>{`Parts of speech: ${meaning.partOfSpeech}`}</Text>
+                                <TouchableOpacity onPress={handlePlay}>
+                                  <Icon
+                                    name="audio"
+                                    size={FONTSIZES.lg}
+                                    color={COLORS.tertiary}
+                                  />
+                                </TouchableOpacity>
                                 <Spacer space={12} />
                                 {meaning.definitions.map(definition => {
                                   return (
-                                    <>
-                                      <Text>{`Definition: ${definition.definition}`}</Text>
+                                    <View
+                                      style={styles.definitionContainer}
+                                      key={definition.example}>
+                                      <Text
+                                        style={
+                                          styles.definition
+                                        }>{`Definition: ${definition.definition}`}</Text>
                                       <Spacer />
                                       {definition.example && (
-                                        <Text>{`Example: ${definition.example}`}</Text>
+                                        <Text
+                                          style={
+                                            styles.example
+                                          }>{`Example: ${definition.example}`}</Text>
                                       )}
-                                    </>
+                                    </View>
                                   );
                                 })}
                                 {meaning.synonyms.length !== 0 && (
-                                  <Text>{`Synonyms: ${meaning.synonyms}`}</Text>
+                                  <Text
+                                    style={[
+                                      styles.definition,
+                                      styles.synonym,
+                                    ]}>{`Synonyms: ${meaning.synonyms}`}</Text>
                                 )}
-                              </>
+                              </View>
                             );
                           })}
                         </View>
-                      </>
+                      </View>
                     );
                   })}
                 </View>
               </>
             )}
+            {/* {error && (
+              <>
+                <Image
+                  source={require('../../assets/images/noData.jpg')}
+                  alt="no-data"
+                  style={styles.image}
+                />
+              </>
+            )} */}
           </View>
         </ScrollView>
       </ScreenWrapper>
--- Vinglish/src/screens/DictionaryScreen/style.ts
+++ Vinglish/src/screens/DictionaryScreen/style.ts
@@ -1,4 +1,6 @@
 import {StyleSheet} from 'react-native';
+import {COLORS} from '../../themes/colors';
+import {FONTSIZES, FONTWEIGHTS} from '../../themes/font';
 
 export const styles = StyleSheet.create({
   searchContainer: {
@@ -9,5 +11,36 @@ export const styles = StyleSheet.create({
     height: 300,
     opacity: 0.3,
     marginTop: 180,
+  },
+  dataContainer: {
+    marginTop: 30,
+    display: 'flex',
+    borderRadius: 8,
+    marginInline: 20,
+  },
+  // inputQuery: {
+  //   textAlign: 'center',
+  //   fontFamily: DefaultFontFamily,
+  //   fontSize: FONTSIZES.subheading,
+  // },
+  phonetic: {
+    fontStyle: 'italic',
+  },
+  definitionContainer: {
+    // backgroundColor: COLORS.tertiary,
+    padding: 5,
+    color: COLORS.text.primary,
+    borderRadius: 8,
+  },
+  definition: {
+    fontSize: FONTSIZES.lg,
+    fontWeight: FONTWEIGHTS.regular,
+  },
+  example: {
+    fontSize: FONTSIZES.lg,
+    fontWeight: FONTWEIGHTS.regular,
+  },
+  synonym: {
+    marginLeft: 7,
   },
 });
